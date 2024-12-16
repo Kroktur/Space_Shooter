f707cbf1d49eeb3302c4f7a9fd6886b09dc878cc
Arthur Blanchet <arthur.blanchet.16@gmail.com>
2024-12-16 16:48:04 +0100

    [dev] test gaming loop

--- Space_Shooter/main.cpp
+++ Space_Shooter/main.cpp
@@ -3,7 +3,32 @@
 
 int main()
 {
+	sf::RenderWindow window(sf::VideoMode(1920, 1080), "SFML works!");
 	sf::CircleShape circle;
+	window.setFramerateLimit(60);
+	sf::Time time;
+	sf::Clock clock;
+	time = clock.getElapsedTime();
+	while (window.isOpen())
+	{
+		window.clear();
+		// process input 
 
+
+		//update
+		if (time.asSeconds() >= 1 / 60)
+		{
+
+
+
+			clock.restart();
+		}
+
+
+		//render
+        
+		window.display();
+
+	}
 
 }
