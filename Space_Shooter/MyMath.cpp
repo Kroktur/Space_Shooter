4db5775b9e22407c152295edc5d772ac0062af90
ValentineFlo <vflorens@gaming.tech>
2024-12-17 10:56:35 +0100

    correction

--- Space_Shooter/MyMath.cpp
+++ Space_Shooter/MyMath.cpp
@@ -9,7 +9,6 @@ int Randomnumber::getrandomnumber(int min, int max)
 	return dis(gen);
 }
 
-Vec2::Vec2(float x_, float y_) : x(x_), y(y_) {}
 
 AABB::AABB(Vec2 Amin_, Vec2 Amax) : Amin(Amin_), Amax(Amax){}
 
