f61a174473504a24104378f956cd72ede0f610dd
Reva <revalecerf@gmail.com>
2024-12-19 10:24:34 +0100

    [Dev] init commit

--- Space_Shooter/main.cpp
+++ Space_Shooter/main.cpp
@@ -10,9 +10,9 @@ int main()
 {
   
     SceneManager sceneManager(1920, 1080, "Test Manager", sf::Style::Fullscreen);
-      sceneManager.push_back(std::make_unique<Menu>(sceneManager.getWindow(), 30.f));
+     sceneManager.push_back(std::make_unique<Menu>(sceneManager.getWindow(), 30.f));
      sceneManager.push_back(std::make_unique<Game>(sceneManager.getWindow(), 60));
-     sceneManager.setCurrentScene(1);
+     sceneManager.setCurrentScene(0);
      sceneManager.Exe();
   
     return 0;
